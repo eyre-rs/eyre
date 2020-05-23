83971ce634043ee18336c9200b8e9cca9d87d829
Duncan Hamill <duncanrhamill@googlemail.com>
2020-05-23 22:23:31 +0100

    Fixed use of std::fmt rather than core::fmt

--- src/fmt.rs
+++ src/fmt.rs
@@ -10,13 +10,13 @@ where
         self.context
             .as_ref()
             .map(|context| context.display(self.error(), f))
-            .unwrap_or_else(|| std::fmt::Display::fmt(self.error(), f))
+            .unwrap_or_else(|| core::fmt::Display::fmt(self.error(), f))
     }
 
     pub(crate) fn debug(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         self.context
             .as_ref()
             .map(|context| context.debug(self.error(), f))
-            .unwrap_or_else(|| std::fmt::Debug::fmt(self.error(), f))
+            .unwrap_or_else(|| core::fmt::Debug::fmt(self.error(), f))
     }
 }
