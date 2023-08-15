4033447a17b49a0e57e75f63e5961be7e45e6e82
Tei Roberts <ten3roberts@gmail.com>
2023-08-16 00:26:00 +0200

    fix: renamed lint

--- src/lib.rs
+++ src/lib.rs
@@ -332,7 +332,8 @@
 #![warn(
     missing_debug_implementations,
     missing_docs,
-    missing_doc_code_examples,
+    // FIXME: this lint is currently nightly only
+    rustdoc::missing_doc_code_examples,
     rust_2018_idioms,
     unreachable_pub,
     bad_style,
