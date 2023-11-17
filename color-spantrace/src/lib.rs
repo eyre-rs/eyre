949e8457773763fe9911f7797188d72f15ceb56d
Jane Losare-Lusby <jlusby@yaah.dev>
2023-11-17 09:35:42 -0800

    remove deprecated lints as of 1.74

--- color-spantrace/src/lib.rs
+++ color-spantrace/src/lib.rs
@@ -65,7 +65,6 @@
     feature(rustdoc_missing_doc_code_examples),
     warn(rustdoc::missing_doc_code_examples)
 )]
-#![cfg_attr(stable, warn(private_in_public))]
 #![warn(
     missing_debug_implementations,
     missing_docs,
