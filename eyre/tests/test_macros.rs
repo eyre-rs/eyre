9b6a4f67ced4d06f3b025e7c5474ebcb029974b7
nori li <50680474+thenorili@users.noreply.github.com>
2023-11-24 17:33:33 -0800

    Fix single-argument ensure test and allow dead code.
    By adding the possibility for polymorphism from an eyre error, the
    previous commit breaks a previous eyre test for single-argument
    ensure!().
    
    This change fixes that test and adds `allow(dead_code)` to the struct
    used for the test for automatically converting to external errors.

--- eyre/tests/test_macros.rs
+++ eyre/tests/test_macros.rs
@@ -41,7 +41,7 @@ fn test_ensure() {
     assert!(f().is_err());
 
     // Tests single-argument `ensure!`
-    let f = || {
+    let f = || -> Result<()> {
         ensure!(v + v == 1);
         Ok(())
     };
@@ -58,6 +58,7 @@ fn test_ensure() {
     assert!(f().is_err());
 }
 
+#[allow(dead_code)]
 struct SomeWrappingErr {
     err: eyre::Error,
 }
